f70b84dceb1eb1f8cad9bc61c37d38d28d03c069
Kevin krzysztof Zak <kzak@e3r12p3.42roma.it>
2023-08-09 16:52:39 +0200

    start

--- inception/srcs/requirements/mariadb/conf/configure.sh
+++ inception/srcs/requirements/mariadb/conf/configure.sh
@@ -4,7 +4,7 @@ then
 	chown -R mysql:mysql /run/mysqld
 fi
 
-if [ ! -d "/var/lib/mysql/$DATABASE_NAME" ]
+if [ ! -d "/var/lib/mysql/mysql" ]
 then
 	chown -R mysql:mysql /var/lib/mysql
 	mysql_install_db --basedir=/usr --datadir=/var/lib/mysql --user=mysql --rpm > /dev/null
@@ -23,20 +23,20 @@ DROP DATABASE test;
 DELETE FROM mysql.db WHERE Db='test';
 DELETE FROM mysql.user WHERE User='root' AND Host NOT IN ('localhost', '127.0.0.1', '::1');
 
-ALTER USER 'root'@'localhost' IDENTIFIED BY '$DB_ROOT_PWD';
+ALTER USER 'root'@'localhost' IDENTIFIED BY '$DB_ADMIN_PASS';
 
-CREATE DATABASE $DATABASE_NAME DEFAULT CHARACTER SET utf8 DEFAULT COLLATE utf8_general_ci;
-CREATE USER '$DATABASE_ADMIN'@'%' IDENTIFIED BY '$DATABASE_ADMIN_PWD';
-GRANT ALL PRIVILEGES ON $DATABASE_NAME.* TO '$DATABASE_ADMIN'@'%';
+CREATE DATABASE $DB_NAME CHARACTER SET utf8 COLLATE utf8_general_ci;
+CREATE USER '$DB_USER'@'%' IDENTIFIED by '$DB_PASS';
+GRANT ALL PRIVILEGES ON $DB_NAME.* TO '$DB_USER'@'%';
+
 FLUSH PRIVILEGES;
 EOF
-
 	/usr/bin/mysqld --user=mysql --bootstrap < $tmp
 	rm -f $tmp
 fi
 
-sed -i 's/skip-networking/# skip-networking/g' /etc/my.cnf.d/mariadb-server.cnf
-sed -i "s/#bind-address=.*/bind-address=0.0.0.0/g" /etc/my.cnf.d/mariadb-server.cnf
+sed -i "s|skip-networking|# skip-networking|g" /etc/my.cnf.d/mariadb-server.cnf
+sed -i "s|.*bind-address\s*=.*|bind-address=0.0.0.0|g" /etc/my.cnf.d/mariadb-server.cnf
 
 echo "MariaDB starting"
 
