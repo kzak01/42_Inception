d72e0e5d6083531c3981fb0dacbe4e03f28ee11f
Kevin krzysztof Zak <kzak@e3r12p3.42roma.it>
2023-08-09 12:41:24 +0200

    prova

---
+++ inception/srcs/requirements/mariadb/conf/configure.sh
@@ -0,0 +1,43 @@
+if [ ! -d "/run/mysqld" ]
+then
+	mkdir -p /run/mysqld
+	chown -R mysql:mysql /run/mysqld
+fi
+
+if [ ! -d "/var/lib/mysql/$DATABASE_NAME" ]
+then
+	chown -R mysql:mysql /var/lib/mysql
+	mysql_install_db --basedir=/usr --datadir=/var/lib/mysql --user=mysql --rpm > /dev/null
+	tmp=`mktemp`
+
+	if [ ! -f "$tmp" ]
+	then
+		return 1
+	fi
+
+	cat << EOF > $tmp
+USE mysql;
+FLUSH PRIVILEGES;
+DELETE FROM mysql.user WHERE User='';
+DROP DATABASE test;
+DELETE FROM mysql.db WHERE Db='test';
+DELETE FROM mysql.user WHERE User='root' AND Host NOT IN ('localhost', '127.0.0.1', '::1');
+
+ALTER USER 'root'@'localhost' IDENTIFIED BY '$DB_ROOT_PWD';
+
+CREATE DATABASE $DATABASE_NAME DEFAULT CHARACTER SET utf8 DEFAULT COLLATE utf8_general_ci;
+CREATE USER '$DATABASE_ADMIN'@'%' IDENTIFIED BY '$DATABASE_ADMIN_PWD';
+GRANT ALL PRIVILEGES ON $DATABASE_NAME.* TO '$DATABASE_ADMIN'@'%';
+FLUSH PRIVILEGES;
+EOF
+
+	/usr/bin/mysqld --user=mysql --bootstrap < $tmp
+	rm -f $tmp
+fi
+
+sed -i 's/skip-networking/# skip-networking/g' /etc/my.cnf.d/mariadb-server.cnf
+sed -i "s/#bind-address=.*/bind-address=0.0.0.0/g" /etc/my.cnf.d/mariadb-server.cnf
+
+echo "MariaDB starting"
+
+exec /usr/bin/mysqld --user=mysql --console
