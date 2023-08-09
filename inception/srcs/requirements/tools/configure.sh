0252d140badf1411e27eb2714592a19f4639f85c
Kevin krzysztof Zak <kzak@e3r12p3.42roma.it>
2023-08-09 13:28:39 +0200

    prova

---
+++ inception/srcs/requirements/tools/configure.sh
@@ -0,0 +1,9 @@
+if [ ! -d "~/data" ];
+then
+	mkdir -p ~/data/website ~/data/wp_database ~/data/logs ~/data/repositories
+fi
+
+if [ `cat /etc/hosts | grep ddelladi.42.fr | wc -l` -eq 0 ];
+then
+	echo "127.0.0.1		ddelladi.42.fr" | sudo tee -a /etc/hosts
+fi
