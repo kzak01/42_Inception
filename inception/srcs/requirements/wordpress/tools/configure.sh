f2fca569ba947e019b59d70ffdfd45f3a853bd0d
Kevin krzysztof Zak <kzak@e3r12p3.42roma.it>
2023-08-22 12:31:35 +0200

    prova

--- inception/srcs/requirements/wordpress/tools/configure.sh
+++ inception/srcs/requirements/wordpress/tools/configure.sh
@@ -13,9 +13,9 @@ else
     # Configure WordPress
     cd /var/www/html/
     wp core download --allow-root
-    wp config create --dbname=$MARIADB_NAME --dbuser=$MARIADB_USER --dbpass=$MARIADB_PASS --dbhost=$MARIADB_HOST --dbcharset="utf8" --dbcollate="utf8_general_ci" --allow-root
+    wp config create --dbname=$MARIADB_DATABASE_NAME --dbuser=$MARIADB_USER --dbpass=$MARIADB_PASS --dbhost=$MARIADB_HOST --dbcharset="utf8" --dbcollate="utf8_general_ci" --allow-root
     wp db create --allow-root
-    wp core install --url=$DOMAIN_NAME --title="Inception" --admin_user=$WORDPRESS_ADMIN_USER --admin_password=$WORDPRESS_ADMIN_PASS --admin_email=$WORDPRESS_ADMIN_MAIL --skip-email --allow-root
+    wp core install --url=$DOMAIN_NAME --title=$WORDPRESS_TITLE --admin_user=$WORDPRESS_ADMIN_USER --admin_password=$WORDPRESS_ADMIN_PASS --admin_email=$WORDPRESS_ADMIN_MAIL --skip-email --allow-root
     wp user create $WORDPRESS_USER $WORDPRESS_MAIL --role=author --user_pass=$WORDPRESS_PASS --allow-root
     # wp theme activate twentytwentythree --allow-root
 
