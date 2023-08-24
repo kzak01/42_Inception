926566ca0470b3d935a77e4151bb56f920848af5
Kevin krzysztof Zak <kzak@e3r12p3.42roma.it>
2023-08-24 12:34:26 +0200

    work_in_progress

--- inception/srcs/requirements/wordpress/tools/configure.sh
+++ inception/srcs/requirements/wordpress/tools/configure.sh
@@ -6,23 +6,32 @@ done
 
 mkdir -p /var/www
 mkdir -p /var/www/html
-cd /var/www/html
+# cd /var/www/html
 wp core download --allow-root
 
 wp config create \
-	--dbname="wordpress" \
+	--dbname=$MARIADB_DATABASE_NAME \
 	--dbuser=$MARIADB_USER \
 	--dbpass=$MARIADB_PASS \
-	--dbhost=$MARIADB_HOST 
+	--dbhost=$MARIADB_HOST \
+	--dbcharset="utf8" \
+	--dbcollate="utf8_general_ci" \
+	--allow-root
+
 wp core install \
-	--url="kzak.42.fr" \
-	--title=$WORDPRESS_TITLE \
+	--url=$DOMAIN_NAME \
+	--title="$WORDPRESS_TITLE" \
 	--admin_user=$WORDPRESS_ADMIN_USER \
 	--admin_password=$WORDPRESS_ADMIN_PASS \
-	--admin_email=$WORDPRESS_ADMIN_MAIL 
+	--admin_email=$WORDPRESS_ADMIN_MAIL \
+	--skip-email \
+	--allow-root
+
 wp user create \
 	$WORDPRESS_USER \
 	$WORDPRESS_MAIL \
+	--role=author \
 	--user_pass=$WORDPRESS_PASS \
+	--allow-root
 
 exec "$@"
