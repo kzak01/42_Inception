5f3effd4014eca22a3afd56e916ac93a7ee1618c
Kevin krzysztof Zak <kzak@e3r12p3.42roma.it>
2023-08-24 12:00:08 +0200

    work_in_progress

--- inception/srcs/requirements/mariadb/tools/configure.sh
+++ inception/srcs/requirements/mariadb/tools/configure.sh
@@ -20,19 +20,19 @@ then
 	cat << EOF > $tmp
 USE mysql;
 FLUSH PRIVILEGES;
+
 DELETE FROM mysql.user WHERE User='';
 DROP DATABASE test;
 DELETE FROM mysql.db WHERE Db='test';
 DELETE FROM mysql.user WHERE User='root' AND Host NOT IN ('localhost', '127.0.0.1', '::1');
 
 ALTER USER 'root'@'localhost' IDENTIFIED BY '$MARIADB_ADMIN_PASS';
-
 CREATE DATABASE $MARIADB_DATABASE_NAME CHARACTER SET utf8 COLLATE utf8_general_ci;
 CREATE USER '$MARIADB_USER'@'%' IDENTIFIED by '$MARIADB_PASS';
 GRANT ALL PRIVILEGES ON $MARIADB_DATABASE_NAME.* TO '$MARIADB_USER'@'%';
-
 FLUSH PRIVILEGES;
 EOF
+
 	/usr/bin/mysqld --user=mysql --bootstrap < $tmp
 	rm -f $tmp
 fi
