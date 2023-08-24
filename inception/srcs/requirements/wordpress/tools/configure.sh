ed820af789fa1536c4829a906dbeb2e7328463e0
Kevin krzysztof Zak <kzak@e3r12p3.42roma.it>
2023-08-24 12:54:44 +0200

    work_in_progress

--- inception/srcs/requirements/wordpress/tools/configure.sh
+++ inception/srcs/requirements/wordpress/tools/configure.sh
@@ -4,34 +4,40 @@ while ! mariadb -h $MARIADB_HOST -u $MARIADB_USER -p$MARIADB_PASS $MARIADB_DATAB
 	sleep 1 
 done 
 
-mkdir -p /var/www
-mkdir -p /var/www/html
-# cd /var/www/html
-wp core download --allow-root
+# mkdir -p /var/www
+# mkdir -p /var/www/html
+cd /var/www/html
 
-wp config create \
-	--dbname=$MARIADB_DATABASE_NAME \
-	--dbuser=$MARIADB_USER \
-	--dbpass=$MARIADB_PASS \
-	--dbhost=$MARIADB_HOST \
-	--dbcharset="utf8" \
-	--dbcollate="utf8_general_ci" \
-	--allow-root
+if [ -f "wp-config.php" ]; then
+	echo "Wordpress good!"
+else
+	echo "Configuring Wordpress"
+	wp core download --allow-root
 
-wp core install \
-	--url=$DOMAIN_NAME \
-	--title="$WORDPRESS_TITLE" \
-	--admin_user=$WORDPRESS_ADMIN_USER \
-	--admin_password=$WORDPRESS_ADMIN_PASS \
-	--admin_email=$WORDPRESS_ADMIN_MAIL \
-	--skip-email \
-	--allow-root
+	wp config create \
+		--dbname=$MARIADB_DATABASE_NAME \
+		--dbuser=$MARIADB_USER \
+		--dbpass=$MARIADB_PASS \
+		--dbhost=$MARIADB_HOST \
+		--dbcharset="utf8" \
+		--dbcollate="utf8_general_ci" \
+		--allow-root
 
-wp user create \
-	$WORDPRESS_USER \
-	$WORDPRESS_MAIL \
-	--role=author \
-	--user_pass=$WORDPRESS_PASS \
-	--allow-root
+	wp core install \
+		--url=$DOMAIN_NAME \
+		--title="$WORDPRESS_TITLE" \
+		--admin_user=$WORDPRESS_ADMIN_USER \
+		--admin_password=$WORDPRESS_ADMIN_PASS \
+		--admin_email=$WORDPRESS_ADMIN_MAIL \
+		--skip-email \
+		--allow-root
+
+	wp user create \
+		$WORDPRESS_USER \
+		$WORDPRESS_MAIL \
+		--role=author \
+		--user_pass=$WORDPRESS_PASS \
+		--allow-root
+fi
 
 exec "$@"
