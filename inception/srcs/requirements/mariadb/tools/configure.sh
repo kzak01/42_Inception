ac0e72ac4f5cd678489970077257286d518efa17
Kevin krzysztof Zak <kzak@e3r12p3.42roma.it>
2023-08-24 15:56:29 +0200

    work_in_progress

--- inception/srcs/requirements/mariadb/tools/configure.sh
+++ inception/srcs/requirements/mariadb/tools/configure.sh
@@ -1,15 +1,19 @@
 #!/bin/sh
 
+# Check if the /run/mysqld directory exists, and create it if not
 if [ ! -d "/run/mysqld" ]
 then
 	mkdir -p /run/mysqld
 	chown -R mysql:mysql /run/mysqld
 fi
 
+# Check if the /var/lib/mysql/mysql directory exists, and create it if not
 if [ ! -d "/var/lib/mysql/mysql" ]
 then
 	chown -R mysql:mysql /var/lib/mysql
 	mysql_install_db --basedir=/usr --datadir=/var/lib/mysql --user=mysql --rpm > /dev/null
+
+	# Create a temporary file to execute SQL commands
 	tmp=`mktemp`
 
 	if [ ! -f "$tmp" ]
@@ -17,6 +21,7 @@ then
 		return 1
 	fi
 
+	# Define SQL commands to set up MariaDB user and database
 	cat << EOF > $tmp
 USE mysql;
 FLUSH PRIVILEGES;
@@ -33,13 +38,16 @@ GRANT ALL PRIVILEGES ON $MARIADB_DATABASE_NAME.* TO '$MARIADB_USER'@'%';
 FLUSH PRIVILEGES;
 EOF
 
+	# Bootstrap MariaDB with the defined SQL commands
 	/usr/bin/mysqld --user=mysql --bootstrap < $tmp
 	rm -f $tmp
 fi
 
+# Modify the MariaDB configuration to allow remote connections
 sed -i "s|skip-networking|# skip-networking|g" /etc/my.cnf.d/mariadb-server.cnf
 sed -i "s|.*bind-address\s*=.*|bind-address=0.0.0.0|g" /etc/my.cnf.d/mariadb-server.cnf
 
 echo "MariaDB starting"
 
+# Start MariaDB with the modified configuration
 exec /usr/bin/mysqld --user=mysql --console
