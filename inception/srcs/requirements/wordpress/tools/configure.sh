6dffcf8b5754ac4badd85f72c2261fc52621a66a
Kevin krzysztof Zak <kzak@e3r12p3.42roma.it>
2023-08-25 10:29:08 +0200

    work_in_progress

--- inception/srcs/requirements/wordpress/tools/configure.sh
+++ inception/srcs/requirements/wordpress/tools/configure.sh
@@ -1,65 +1,17 @@
-# # #!/bin/sh
+# #!/bin/sh
 
-# while ! mariadb -h $MARIADB_HOST -u $MARIADB_USER -p$MARIADB_PASS $MARIADB_DATABASE_NAME --silent; do
-# 	sleep 1 
-# done 
-
-# cd /var/www/html
-
-# if [ -f "wp-config.php" ]; then
-# 	echo "Wordpress good!"
-# else
-# 	echo "Configuring Wordpress"
-# 	wp core download --allow-root
-
-# 	wp config create \
-# 		--dbname=$MARIADB_DATABASE_NAME \
-# 		--dbuser=$MARIADB_USER \
-# 		--dbpass=$MARIADB_PASS \
-# 		--dbhost=$MARIADB_HOST \
-# 		--dbcharset="utf8" \
-# 		--dbcollate="utf8_general_ci" \
-# 		--allow-root
-
-# 	wp core install \
-# 		--url=$DOMAIN_NAME \
-# 		--title="$WORDPRESS_TITLE" \
-# 		--admin_user=$WORDPRESS_ADMIN_USER \
-# 		--admin_password=$WORDPRESS_ADMIN_PASS \
-# 		--admin_email=$WORDPRESS_ADMIN_MAIL \
-# 		--skip-email \
-# 		--allow-root
-
-# 	wp user create \
-# 		$WORDPRESS_USER \
-# 		$WORDPRESS_MAIL \
-# 		--role=author \
-# 		--user_pass=$WORDPRESS_PASS \
-# 		--allow-root
-# fi
-
-# /usr/sbin/php-fpm81 -F
-
-#!/bin/sh
-
-# Wait until MariaDB is reachable before proceeding
 while ! mariadb -h $MARIADB_HOST -u $MARIADB_USER -p$MARIADB_PASS $MARIADB_DATABASE_NAME --silent; do
 	sleep 1 
 done 
 
-# Change the current directory to the WordPress installation directory
 cd /var/www/html
 
-# Check if the wp-config.php file already exists
 if [ -f "wp-config.php" ]; then
-	echo "Wordpress already configured!"
+	echo "Wordpress good!"
 else
 	echo "Configuring Wordpress"
-
-	# Download WordPress core files
 	wp core download --allow-root
 
-	# Create a wp-config.php file with provided database configuration
 	wp config create \
 		--dbname=$MARIADB_DATABASE_NAME \
 		--dbuser=$MARIADB_USER \
@@ -69,7 +21,6 @@ else
 		--dbcollate="utf8_general_ci" \
 		--allow-root
 
-	# Install WordPress with provided setup information
 	wp core install \
 		--url=$DOMAIN_NAME \
 		--title="$WORDPRESS_TITLE" \
@@ -79,7 +30,6 @@ else
 		--skip-email \
 		--allow-root
 
-	# Create a new user with the specified role and credentials
 	wp user create \
 		$WORDPRESS_USER \
 		$WORDPRESS_MAIL \
@@ -88,5 +38,4 @@ else
 		--allow-root
 fi
 
-# Start PHP-FPM service in foreground mode
 /usr/sbin/php-fpm81 -F
