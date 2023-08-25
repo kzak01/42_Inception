c008d20ba677a285bc3446845abec41c54ee8486
Kevin krzysztof Zak <kzak@e3r12p3.42roma.it>
2023-08-25 10:16:42 +0200

    work_in_progress

--- inception/srcs/requirements/wordpress/tools/configure.sh
+++ inception/srcs/requirements/wordpress/tools/configure.sh
@@ -1,17 +1,23 @@
-# #!/bin/sh
+#!/bin/sh
 
+# Wait until MariaDB is reachable before proceeding
 while ! mariadb -h $MARIADB_HOST -u $MARIADB_USER -p$MARIADB_PASS $MARIADB_DATABASE_NAME --silent; do
 	sleep 1 
 done 
 
+# Change the current directory to the WordPress installation directory
 cd /var/www/html
 
+# Check if the wp-config.php file already exists
 if [ -f "wp-config.php" ]; then
-	echo "Wordpress good!"
+	echo "Wordpress already configured!"
 else
 	echo "Configuring Wordpress"
+
+	# Download WordPress core files
 	wp core download --allow-root
 
+	# Create a wp-config.php file with provided database configuration
 	wp config create \
 		--dbname=$MARIADB_DATABASE_NAME \
 		--dbuser=$MARIADB_USER \
@@ -21,6 +27,7 @@ else
 		--dbcollate="utf8_general_ci" \
 		--allow-root
 
+	# Install WordPress with provided setup information
 	wp core install \
 		--url=$DOMAIN_NAME \
 		--title="$WORDPRESS_TITLE" \
@@ -30,6 +37,7 @@ else
 		--skip-email \
 		--allow-root
 
+	# Create a new user with the specified role and credentials
 	wp user create \
 		$WORDPRESS_USER \
 		$WORDPRESS_MAIL \
@@ -38,5 +46,5 @@ else
 		--allow-root
 fi
 
-# exec "$@"
+# Start PHP-FPM service in foreground mode
 /usr/sbin/php-fpm81 -F
